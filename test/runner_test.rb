455105bbc850887b478011f08dd24b2e8eb31dbf
Hrvoje Šimić <shime.ferovac@gmail.com>
2014-08-11 22:29:38 +0200

    fix runner test

--- test/runner_test.rb
+++ test/runner_test.rb
@@ -6,8 +6,8 @@ require './test/helper'
 describe 'Verdi::Runner' do
   describe 'fetch' do
     it 'saves the package to disk' do
-      runner = Verdi::Runner.new(['minitest'])
-      File.basename(runner.fetch('4.0')).must_equal('minitest-4.0.0.gem')
+      runner = Verdi::Runner.new({})
+      File.basename(runner.fetch('minitest-4.0')).must_equal('minitest-4.0.0.gem')
     end
   end
 end
